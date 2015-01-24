49c7cf7df03cf74ebc5e679eb50ab4ae4f62bf70
Jesse Beder <jbeder@gmail.com>
2015-01-24 16:29:57 -0600

    Refactor plain scalar validation in the emitter to precompute the invalid regexes

---
+++ util/sandbox.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "yaml-cpp/emitterstyle.h"
+#include "yaml-cpp/eventhandler.h"
+#include "yaml-cpp/yaml.h"  // IWYU pragma: keep
+
+class NullEventHandler : public YAML::EventHandler {
+ public:
+  typedef YAML::Mark Mark;
+  typedef YAML::anchor_t anchor_t;
+
+  NullEventHandler() {}
+
+  virtual void OnDocumentStart(const Mark&) {}
+  virtual void OnDocumentEnd() {}
+  virtual void OnNull(const Mark&, anchor_t) {}
+  virtual void OnAlias(const Mark&, anchor_t) {}
+  virtual void OnScalar(const Mark&, const std::string&, anchor_t,
+                        const std::string&) {}
+  virtual void OnSequenceStart(const Mark&, const std::string&, anchor_t,
+                               YAML::EmitterStyle::value style) {}
+  virtual void OnSequenceEnd() {}
+  virtual void OnMapStart(const Mark&, const std::string&, anchor_t,
+                          YAML::EmitterStyle::value style) {}
+  virtual void OnMapEnd() {}
+};
+
+int main() {
+  std::stringstream stream("---{header: {id: 1");
+  YAML::Parser parser(stream);
+  //    parser.PrintTokens(std::cout);
+  NullEventHandler handler;
+  parser.HandleNextDocument(handler);
+  return 0;
+}
