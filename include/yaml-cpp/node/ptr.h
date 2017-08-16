bba5a816b0dd68c30fbe49d2a24ea368724812bc
graehl <graehl@gmail.com>
2017-08-16 08:29:59 -0700

    Node hash() and id() for faster is()-type sets
    A common use case is to transform each Node once even when *1 &1 DAG
    sharing exists in the input. This needs an 'already processed' set,
    which, using only is() would be quadratic in this use case. hashable
    ids (exposing the underlying pointer used by is) makes it linear time.
    
    For performance, part of node/detail/node.h is effectively exposed - the
    fact that it has a shared_ptr<node_ref> as its first member.
    
    Alternatively the implementation of id() could be made non-inline so the
    full definition of detail::node could be used.

--- include/yaml-cpp/node/ptr.h
+++ include/yaml-cpp/node/ptr.h
@@ -18,6 +18,26 @@ class node_data;
 class memory;
 class memory_holder;
 
+class node_ref_id {
+protected:
+  typedef std::shared_ptr<void> Ptr;
+  Ptr m_pRef;
+  node_ref_id(Ptr && p) : m_pRef((Ptr &&)p) {}
+  node_ref_id(const node_ref_id&) = delete;
+  node_ref_id& operator=(const node_ref_id&) = delete;
+  detail::node_ref& nref() const { return *ref(); }
+public:
+  detail::node_ref* ref() const { return (detail::node_ref*)m_pRef.get(); }
+  const void* id() const {
+    return m_pRef.get();
+  }
+};
+
+inline node_ref_id const* ref_id(node const* node) {
+  // this could be static_cast if we pulled in the full definition of node
+  return reinterpret_cast<node_ref_id const*>(node);
+}
+
 typedef std::shared_ptr<node> shared_node;
 typedef std::shared_ptr<node_ref> shared_node_ref;
 typedef std::shared_ptr<node_data> shared_node_data;
