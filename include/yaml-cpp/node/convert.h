e20be84471ee10a413b89110a19ef63200f5eee1
Sam4uk <sto.ukr.net@gmail.com>
2022-02-06 16:29:54 +0200

    -Werror=old-style-cast
    use of old-style cast to ‘T’ [-Werror=old-style-cast]
      131 | rhs = (T)num;

--- include/yaml-cpp/node/convert.h
+++ include/yaml-cpp/node/convert.h
@@ -128,7 +128,7 @@ ConvertStreamTo(std::stringstream& stream, T& rhs) {
   if ((stream >> std::noskipws >> num) && (stream >> std::ws).eof()) {
     if (num >= (std::numeric_limits<T>::min)() &&
         num <= (std::numeric_limits<T>::max)()) {
-      rhs = (T)num;
+      rhs = static_cast<T>(num);
       return true;
     }
   }
