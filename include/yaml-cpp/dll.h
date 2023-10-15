93c93cd3113aae4b82e9ccc8e6bdab58b0562f7d
KampotKaca <62181228+KampotKaca@users.noreply.github.com>
2023-10-15 15:52:30 +0400

    Update dll.h

--- include/yaml-cpp/dll.h
+++ include/yaml-cpp/dll.h
@@ -7,40 +7,40 @@
 // Definition yaml_cpp_EXPORTS using to building YAML-CPP as dll/so library
 // (definition created by CMake or defined manually)
 
-#ifdef YAML_CPP_STATIC_DEFINE
+//#ifdef YAML_CPP_STATIC_DEFINE
 #  define YAML_CPP_API
 #  define YAML_CPP_NO_EXPORT
-#else
-#  if defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)
-#    ifndef YAML_CPP_API
-#      ifdef yaml_cpp_EXPORTS
-         /* We are building this library */
-#        pragma message( "Defining YAML_CPP_API for DLL export" )
-#        define YAML_CPP_API __declspec(dllexport)
-#      else
-         /* We are using this library */
-#        pragma message( "Defining YAML_CPP_API for DLL import" )
-#        define YAML_CPP_API __declspec(dllimport)
-#      endif
-#    endif
-#    ifndef YAML_CPP_NO_EXPORT
-#      define YAML_CPP_NO_EXPORT
-#    endif
-#  else /* No _MSC_VER */
-#    ifndef YAML_CPP_API
-#      ifdef yaml_cpp_EXPORTS
-         /* We are building this library */
-#        define YAML_CPP_API __attribute__((visibility("default")))
-#      else
-         /* We are using this library */
-#        define YAML_CPP_API __attribute__((visibility("default")))
-#      endif
-#    endif
-#    ifndef YAML_CPP_NO_EXPORT
-#      define YAML_CPP_NO_EXPORT __attribute__((visibility("hidden")))
-#    endif
-#  endif /* _MSC_VER */
-#endif   /* YAML_CPP_STATIC_DEFINE */
+//#else
+//#  if defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)
+//#    ifndef YAML_CPP_API
+//#      ifdef yaml_cpp_EXPORTS
+//         /* We are building this library */
+//#        pragma message( "Defining YAML_CPP_API for DLL export" )
+//#        define YAML_CPP_API __declspec(dllexport)
+//#      else
+//         /* We are using this library */
+//#        pragma message( "Defining YAML_CPP_API for DLL import" )
+//#        define YAML_CPP_API __declspec(dllimport)
+//#      endif
+//#    endif
+//#    ifndef YAML_CPP_NO_EXPORT
+//#      define YAML_CPP_NO_EXPORT
+//#    endif
+//#  else /* No _MSC_VER */
+//#    ifndef YAML_CPP_API
+//#      ifdef yaml_cpp_EXPORTS
+//         /* We are building this library */
+//#        define YAML_CPP_API __attribute__((visibility("default")))
+//#      else
+//         /* We are using this library */
+//#        define YAML_CPP_API __attribute__((visibility("default")))
+//#      endif
+//#    endif
+//#    ifndef YAML_CPP_NO_EXPORT
+//#      define YAML_CPP_NO_EXPORT __attribute__((visibility("hidden")))
+//#    endif
+//#  endif /* _MSC_VER */
+//#endif   /* YAML_CPP_STATIC_DEFINE */
 
 #ifndef YAML_CPP_DEPRECATED
 #  ifdef _MSC_VER
