acf2e70f0cf5b65c2a45eed232439c74f12c3ce8
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-07-30 11:48:12 +0200

    fix: add YAML_CPP_API for windows-shared library builds

--- include/yaml-cpp/fptostring.h
+++ include/yaml-cpp/fptostring.h
@@ -4,13 +4,15 @@
 #ifndef YAML_H_FP_TO_STRING
 #define YAML_H_FP_TO_STRING
 
+#include "yaml-cpp/dll.h"
+
 #include <string>
 
 namespace YAML {
 // "precision = 0" refers to shortest known unique representation of the value
-std::string FpToString(float v, size_t precision = 0);
-std::string FpToString(double v, size_t precision = 0);
-std::string FpToString(long double v, size_t precision = 0);
+YAML_CPP_API std::string FpToString(float v, size_t precision = 0);
+YAML_CPP_API std::string FpToString(double v, size_t precision = 0);
+YAML_CPP_API std::string FpToString(long double v, size_t precision = 0);
 }
 
 #endif
