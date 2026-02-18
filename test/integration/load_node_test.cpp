8a99be082d384ecddebb5d484235a65e2cd4e7ad
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2026-02-18 21:25:26 +0100

    test: demonstrate error on multiple empty documents

--- test/integration/load_node_test.cpp
+++ test/integration/load_node_test.cpp
@@ -375,9 +375,19 @@ TEST(NodeTest, InfiniteLoopNodes) {
   EXPECT_THROW(LoadAll(R"(,)"), ParserException);
 }
 
-TEST(NodeTest, MultipleDocuments) {
+TEST(NodeTest, MultipleDocumentsBeginning) {
   std::vector<Node> docs = LoadAll("\n---\n---\nA\n");
   EXPECT_EQ(docs.size(), 2);
+}
+
+TEST(NodeTest, MultipleDocumentsEnds) {
+  std::vector<Node> docs = LoadAll("\n...\nA\n...\n");
+  EXPECT_EQ(docs.size(), 2);
+}
+
+TEST(NodeTest, MultipleDocumentsEndsWithEmptyDocs) {
+  std::vector<Node> docs = LoadAll("\n...\nA\n...\n...\nB\n...");
+  EXPECT_EQ(docs.size(), 4);
 }
 
 struct NewLineStringsTestCase {
