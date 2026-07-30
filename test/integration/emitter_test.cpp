bf5386124134baf057ec616cf10a01098e102f9c
Alex Prabhat Bara <alexpbara@gmail.com>
2026-07-30 19:07:38 +0530

    Apply indentation before emitting comments on new lines (#1426)
    * apply indentation before emitting comments on new lines
    
    * fix for failing tests and formatting
    
    * additional unit tests
    
    log: patch: apply indentation when emitting comments on new lines

--- test/integration/emitter_test.cpp
+++ test/integration/emitter_test.cpp
@@ -1946,5 +1946,68 @@ TEST_F(EmitterTest, ShowTrailingZero) {
 - .nan)");
 }
 
+TEST_F(EmitterTest, CommentInsideMapValueIsIndented) {
+  out << YAML::BeginMap << YAML::Key << "foo" << YAML::BeginMap
+      << YAML::Comment("Comment") << YAML::Key << "bar" << YAML::Value << true
+      << YAML::EndMap << YAML::EndMap;
+
+  ExpectEmit(
+      "foo:\n"
+      "  # Comment\n"
+      "  bar: true");
+}
+
+TEST_F(EmitterTest, CommentInsideDoubleNestedMapIsIndented) {
+  out << YAML::BeginMap << YAML::Key << "map1" << YAML::Value << YAML::BeginMap
+      << YAML::Key << "map2" << YAML::Value << YAML::BeginMap
+      << YAML::Comment("nested comment") << YAML::Key << "foo" << YAML::Value
+      << "bar" << YAML::EndMap << YAML::EndMap << YAML::EndMap;
+
+  ExpectEmit(
+      "map1:\n"
+      "  map2:\n"
+      "    # nested comment\n"
+      "    foo: bar");
+}
+
+TEST_F(EmitterTest, CommentAtEndOfDoubleNestedMapIsIndented) {
+  out << YAML::BeginMap << YAML::Key << "map1" << YAML::Value << YAML::BeginMap
+      << YAML::Key << "map2" << YAML::Value << YAML::BeginMap << YAML::Key
+      << "foo" << YAML::Value << "bar" << YAML::Newline
+      << YAML::Comment("nested comment at the end") << YAML::EndMap
+      << YAML::EndMap << YAML::EndMap;
+
+  ExpectEmit(
+      "map1:\n"
+      "  map2:\n"
+      "    foo: bar\n"
+      "    # nested comment at the end");
+}
+
+TEST_F(EmitterTest, CommentAfterNestedMapUsesParentIndentation) {
+  out << YAML::BeginMap << YAML::Key << "map1" << YAML::Value << YAML::BeginMap
+      << YAML::Key << "map2" << YAML::Value << YAML::BeginMap << YAML::Key
+      << "foo" << YAML::Value << "bar" << YAML::EndMap << YAML::Newline
+      << YAML::Comment("nested comment outside of map2") << YAML::EndMap
+      << YAML::EndMap;
+
+  ExpectEmit(
+      "map1:\n"
+      "  map2:\n"
+      "    foo: bar\n"
+      "  # nested comment outside of map2");
+}
+
+TEST_F(EmitterTest, CommentInsideFlowMapIsUnaffected) {
+  out << YAML::BeginMap << YAML::Key << "some_map" << YAML::Value << YAML::Flow
+      << YAML::BeginMap << YAML::Key << "foo" << YAML::Value << "bar"
+      << YAML::Comment("comment") << YAML::Key << "key2" << YAML::Value
+      << "value2" << YAML::EndMap << YAML::EndMap;
+
+  ExpectEmit(
+      "some_map: {foo: bar,  # comment\n"
+      "key2: value2}");
+}
+
 }  // namespace
 }  // namespace YAML
