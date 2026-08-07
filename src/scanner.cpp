3696529f87bd5bf11c9a97ba0ca08c8b74624c61
sxrzh <adfhkjafsdk@outlook.com>
2026-08-07 21:18:59 +0800

    fix indent-stack underflow for literal scalar with tag

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -177,7 +177,15 @@ void Scanner::ScanNextToken() {
     //  scalar
     if (!m_simpleKeys.empty() &&
       m_simpleKeys.top().pKey->status == Token::UNVERIFIED) {
+      // if the top of the indents does not match the unverified simple key,
+      // just invalidate the simple key and do not pop indent to avoid crash.
+      // eg: an unverified key on a previous line, like issue #1475.
+      if (m_simpleKeys.top().pIndent && !m_indents.empty() &&
+          m_indents.top() == m_simpleKeys.top().pIndent) {
         PopIndent();
+      } else {
+        InvalidateSimpleKey();
+      }
     }
     return ScanBlockScalar();
   }
