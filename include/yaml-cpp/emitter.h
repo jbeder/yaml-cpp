2f64971599ed05b15e2945949e834e96b6288afb
Rosen Penev <rosenp@gmail.com>
2026-08-10 03:54:18 -0700

    [clang-tidy] several const changes (#896)
    * [clang-tidy] remove pointless const
    
    Found with readability-const-return-type
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    
    * [clang-tidy] make several member functions const
    
    log: tidying code
    Found with readability-make-member-function-const
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    
    * [clang-tidy] convert member functions to static
    
    Found with readability-convert-member-functions-to-static
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    
    * clang-tidy: do not use compare
    
    Found with readability-string-compare
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    
    ---------
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Co-authored-by: Simon Gene Gottlieb <simon@gottliebtfreitag.de>

--- include/yaml-cpp/emitter.h
+++ include/yaml-cpp/emitter.h
@@ -50,7 +50,7 @@ class YAML_CPP_API Emitter {
 
   // state checking
   bool good() const;
-  const std::string GetLastError() const;
+  std::string GetLastError() const;
 
   // global setters
   bool SetOutputCharset(EMITTER_MANIP value);
@@ -136,7 +136,6 @@ class YAML_CPP_API Emitter {
 
   const char* ComputeFullBoolName(bool b) const;
   const char* ComputeNullName() const;
-  bool CanEmitNewline() const;
 
  private:
   std::unique_ptr<EmitterState> m_pState;
