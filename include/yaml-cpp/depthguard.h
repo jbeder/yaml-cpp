0cae801bb7929835d82116b37f17ea8938cbca31
harleywilsoneng <harleywilsoneng@users.noreply.github.com>
2026-08-12 10:18:33 +0800

    feat: add IWYU pragmas for public/private headers (#1386)
    Signed-off-by: harleywilsoneng <harleywilsoneng@users.noreply.github.com>

--- include/yaml-cpp/depthguard.h
+++ include/yaml-cpp/depthguard.h
@@ -1,11 +1,20 @@
 #ifndef DEPTH_GUARD_H_00000000000000000000000000000000000000000000000000000000
 #define DEPTH_GUARD_H_00000000000000000000000000000000000000000000000000000000
 
+
+
+
 #if defined(_MSC_VER) ||                                            \
     (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
      (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
 #pragma once
+
+
 #endif
+
+// IWYU pragma: private, include "yaml-cpp/yaml.h"
+// IWYU pragma: friend "yaml-cpp/.*"
+
 
 #include "exceptions.h"
 
